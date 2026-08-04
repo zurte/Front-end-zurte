f9a0bb67b57503fe977ea515904d499806349c9e
Gonn-Freecs <kauangonncalv3s@gmail.com>
2026-08-03 21:19:10 -0300

    AAAAAAAAAAAAAAAAAAAAAAAAAAA

--- exercicio 7/js/script.js
+++ exercicio 7/js/script.js
@@ -1,46 +1,46 @@
 const produtos = [
     {
-        nome: "chinelo quadradis"
-        preco: "$20,00"
-        Imagem: ""
-        descricao: "Chinelo quadradis"
+        nome: "chinelo quadradis",
+        preco: "$20,00",
+        imagem: "fsdfdssff",
+        descricao: "Chinelo quadradis",
         
     },
         
     {    
-        nome: "Miojo"
-        preco: "$5,00"
-        imagem:
-        descricao: "O classic miojis né"
+        nome: "Miojo",
+        preco: "$5,00",
+        imagem: "fsfdssdf",
+        descricao: "O classic miojis né",
     },
 
     {
-        nome:  "Jarra"
-        preco: "$15,00"
-        imagem:
-        descricao: "É uma jarra"
+        nome:  "Jarra",
+        preco: "$15,00",
+        imagem: "fdsfssdf",
+        descricao: "É uma jarra",
     },
  
     {
-        nome:  "Camisa do space jam"
-        preco: "$30,00"
-        imagem:
-        descricao: "Essa camisa é boladex e vale a pena comprar"
+        nome:  "Camisa do space jam",
+        preco: "$30,00",
+        imagem: "sdfsdfsdf",
+        descricao: "Essa camisa é boladex e vale a pena comprar",
     },    
 
     {
-        nome: "Mangá de bleach"
-        preco: "$10,00"
-        imagem:
-        descricao: "Esse anime é bolado"
+        nome: "Mangá de bleach",
+        preco: "$10,00",
+        imagem: "sdfsdfe",
+        descricao: "Esse anime é bolado",
     }
 ];
-const catalogo = document.getElementById("catalogo");
+const listaProdutos = document.getElementById("listaProdutos");
 
 produtos.forEach(produtos => {
-    catalogo.innerHTML += `
+    listaProdutos.innerHTML += `
     <div class="card" style="width: 18rem;">
-  <img src="${produtos.imagem}..." class="card-img-top" alt="${produtos.nome}...">
+  <img src="${produtos.imagem}" class="card-img-top" alt="${produtos.nome}">
   <div class="card-body">
     <h5 class="card-title">${produtos.nome}</h5>
     <p class="card-text">${produtos.descricao}</p>
@@ -49,3 +49,5 @@ produtos.forEach(produtos => {
 </div>
 `;
 });
+
+console.log("JS carregado!");
